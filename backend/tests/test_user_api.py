824f8df12a61196b12d8024f45fdf40a6f8caf1f
Ralf Vench Billones <ralfvenchbillones@gmail.com>
2021-12-17 01:00:23 +0800

    add api test

--- backend/tests/test_user_api.py
+++ backend/tests/test_user_api.py
@@ -20,7 +20,7 @@ def test_register_user_fail(test_app):
     """
     Given: user already exist
     when: /user/register api call
-    then: raise an error
+    then: return HTTP error
     """
     data = json.dumps({"username": "ralf", "password": "123"})
 
@@ -28,3 +28,88 @@ def test_register_user_fail(test_app):
     response = test_app.post('/user/register', data=data)
 
     assert 409 == response.status_code
+
+
+def test_login_success(test_app):
+    """
+    Given: existed user
+    when: /user/login api call
+    then: return user information
+    """
+    data = json.dumps({"username": "ralf", "password": "123"})
+    test_app.post('/user/register', data=data)
+    response = test_app.post('/user/login', data=data)
+
+    assert 200 == response.status_code
+    assert 'ralf' == response.json()['username']
+
+
+def test_login_fail(test_app):
+    """
+    Given: user did not exist
+    when: /user/login api call
+    then: return HTTP error
+    """
+    data = json.dumps({"username": "ralf123123", "password": "123"})
+    response = test_app.post('/user/login', data=data)
+
+    assert 404 == response.status_code
+
+
+def test_update_user(test_app):
+    """
+    Given: existed user and valid data for update
+    when: /user/{id}/update api call
+    then: return user information
+    """
+    data = json.dumps({"username": "updateralf",
+                       "password": "123",
+                       "email": "test@mail.com"})
+
+    user_id = test_app.post('/user/register', data=data).json()['id']
+    response = test_app.put(f'/user/{user_id}/update', data=data)
+
+    assert 200 == response.status_code
+
+
+def test_update_user_fail(test_app):
+    """
+    Given: non-existed user
+    when: /user/{id}/update api call
+    then: return HTTP error
+    """
+    data = json.dumps({"username": "updateralf",
+                       "password": "123",
+                       "email": "test@mail.com"})
+    user_id = 123123
+    response = test_app.put(f'/user/{user_id}/update', data=data)
+
+    assert 404 == response.status_code
+
+
+def test_get_user(test_app):
+    """
+    Given: existed user
+    when: /user/{id} api call
+    then: return user information
+    """
+    data = json.dumps({"username": "ralfget", "password": "123"})
+
+    user_id = test_app.post('/user/register', data=data).json()['id']
+    response = test_app.get(f'/user/{user_id}')
+
+    assert 200 == response.status_code
+    assert 'ralfget' == response.json()['username']
+
+
+def test_get_user_fail(test_app):
+    """
+    Given: non-existed user
+    when: /user/{id} api call
+    then: return user information
+    """
+
+    user_id = 132312
+    response = test_app.get(f'/user/{user_id}')
+
+    assert 404 == response.status_code
